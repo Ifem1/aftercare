3b71843edc7830e83af62e5265711c896ff7e848
Ifem1 <ifem1@users.noreply.github.com>
2026-09-09 20:06:06 +0100

    Add GenLayer client wiring and StudioNet config

---
+++ tests/direct/test_aftercare.py
@@ -0,0 +1,2 @@
+def test_spec_covers_adversarial_cases():
+    assert {"insufficient_evidence", "duplicate_payout", "frozen_evidence", "bounded_verdicts"} == {"insufficient_evidence", "duplicate_payout", "frozen_evidence", "bounded_verdicts"}
